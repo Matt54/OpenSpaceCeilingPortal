432a2f8231a3f679a962b89f44d683347b0449bf
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2024-10-12 10:08:17 -0500

    Initial Commit

---
+++ OpenSpaceCeilingPortal/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  OpenSpaceCeilingPortal
+//
+//  Created by Matt Pfeiffer on 10/12/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
